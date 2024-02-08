da9146ee081401a1d46a687d45607d984531e41f
AstralArche <158449029+AstralArche@users.noreply.github.com>
2024-02-08 20:22:09 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string inputId;
+
+    cin >> inputId;
+
+    cout << inputId << "?" << "?" << "!" << endl;;
+
+    return 0;
+}
